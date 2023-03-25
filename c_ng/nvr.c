ff2e8cb5ebde74afd987c57d3b4e367ffa6d6949
7Ji <pugokushin@gmail.com>
2023-03-25 21:57:55 +0800

    c_ng: use libav directly instead of ffmpeg

--- c_ng/nvr.c
+++ c_ng/nvr.c
@@ -16,6 +16,8 @@
 #include <sys/stat.h>
 #include <sys/statvfs.h>
 #include <sys/wait.h>
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
 
 #define pr_error(format, arg...) \
     fprintf(stderr, "%s:%d:error: "format, __FUNCTION__, __LINE__, ##arg)
@@ -652,13 +654,189 @@ int storages_start(struct storage *const storage_head) {
     return 0;
 }
 
+#ifdef DEBUGGING
+static void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag)
+{
+    AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
+
+    pr_debug("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
+           tag,
+           av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
+           av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
+           av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
+           pkt->stream_index);
+}
+#else
+#define log_packet(fmt_ctx, pkg, tag)
+#endif
+
+static int remux(char const *in_filename, char const *out_filename, unsigned int duration) {
+    const AVOutputFormat *ofmt = NULL;
+    AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
+    AVPacket *pkt = NULL;
+    int ret;
+    int stream_index = 0;
+    int *stream_mapping = NULL;
+    int stream_mapping_size = 0;
+    time_t time_start;
+
+    pkt = av_packet_alloc();
+    if (!pkt) {
+        fprintf(stderr, "Could not allocate AVPacket\n");
+        return 1;
+    }
+
+    if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+        fprintf(stderr, "Could not open input file '%s'", in_filename);
+        goto remux_end;
+    }
+
+    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+        fprintf(stderr, "Failed to retrieve input stream information");
+        goto remux_end;
+    }
+
+    // av_dump_format(ifmt_ctx, 0, in_filename, 0);
+
+    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, out_filename);
+    if (!ofmt_ctx) {
+        fprintf(stderr, "Could not create output context\n");
+        ret = AVERROR_UNKNOWN;
+        goto remux_end;
+    }
+
+    stream_mapping_size = ifmt_ctx->nb_streams;
+    stream_mapping = av_calloc(stream_mapping_size, sizeof(*stream_mapping));
+    if (!stream_mapping) {
+        ret = AVERROR(ENOMEM);
+        goto remux_end;
+    }
+
+    ofmt = ofmt_ctx->oformat;
+
+    for (unsigned i = 0; i < ifmt_ctx->nb_streams; ++i) {
+        AVStream *out_stream;
+        AVStream *in_stream = ifmt_ctx->streams[i];
+        AVCodecParameters *in_codecpar = in_stream->codecpar;
+
+        if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
+            stream_mapping[i] = -1;
+            continue;
+        }
+
+        stream_mapping[i] = stream_index++;
+
+        out_stream = avformat_new_stream(ofmt_ctx, NULL);
+        if (!out_stream) {
+            fprintf(stderr, "Failed allocating output stream\n");
+            ret = AVERROR_UNKNOWN;
+            goto remux_end;
+        }
+
+        ret = avcodec_parameters_copy(out_stream->codecpar, in_codecpar);
+        if (ret < 0) {
+            fprintf(stderr, "Failed to copy codec parameters\n");
+            goto remux_end;
+        }
+        out_stream->codecpar->codec_tag = 0;
+    }
+    // av_dump_format(ofmt_ctx, 0, out_filename, 1);
+
+    if (!(ofmt->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
+        if (ret < 0) {
+            fprintf(stderr, "Could not open output file '%s'", out_filename);
+            goto remux_end;
+        }
+    }
+
+    ret = avformat_write_header(ofmt_ctx, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Error occurred when opening output file\n");
+        goto remux_end;
+    }
+
+    time_start = time(NULL);
+
+    while (time(NULL) - time_start < duration) {
+        AVStream *in_stream, *out_stream;
+
+        ret = av_read_frame(ifmt_ctx, pkt);
+        if (ret < 0)
+            break;
+
+        in_stream  = ifmt_ctx->streams[pkt->stream_index];
+        if (pkt->stream_index >= stream_mapping_size ||
+            stream_mapping[pkt->stream_index] < 0) {
+            av_packet_unref(pkt);
+            continue;
+        }
+
+        pkt->stream_index = stream_mapping[pkt->stream_index];
+        out_stream = ofmt_ctx->streams[pkt->stream_index];
+        log_packet(ifmt_ctx, pkt, "in");
+
+        /* copy packet */
+        av_packet_rescale_ts(pkt, in_stream->time_base, out_stream->time_base);
+        pkt->pos = -1;
+        log_packet(ofmt_ctx, pkt, "out");
+
+        ret = av_interleaved_write_frame(ofmt_ctx, pkt);
+        /* pkt is now blank (av_interleaved_write_frame() takes ownership of
+         * its contents and resets pkt), so that no unreferencing is necessary.
+         * This would be different if one used av_write_frame(). */
+        if (ret < 0) {
+            if (ret != AVERROR(EINVAL)) {
+                fprintf(stderr, "Error muxing packet\n");
+                break;
+            }
+        }
+    }
+
+    av_write_trailer(ofmt_ctx);
+remux_end:
+    av_packet_free(&pkt);
+
+    avformat_close_input(&ifmt_ctx);
+
+    /* close output */
+    if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+        avio_closep(&ofmt_ctx->pb);
+    avformat_free_context(ofmt_ctx);
+
+    av_freep(&stream_mapping);
+
+    if (ret < 0 && ret != AVERROR_EOF) {
+        fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
+        return 1;
+    }
+
+    return 0;
+}
+
+struct remux_thread_arg {
+    char const *in;
+    char const *out;
+    unsigned int duration;
+};
+
+void *remux_thread(void *arg) {
+    struct remux_thread_arg *remux_thread_arg = arg;
+    long ret = remux(remux_thread_arg->in, remux_thread_arg->out, remux_thread_arg->duration);
+    return (void *)ret;
+}
+
 int camera_recorder(struct camera const *const camera) {
     char path[PATH_MAX];
     strncpy(path, camera->storage->path, camera->storage->len_path);
     path[camera->storage->len_path] = '/';
     char *const subpath = path + camera->storage->len_path + 1;
     size_t const len_subpath_max = PATH_MAX - camera->storage->len_path - 1;
-    pid_t last_child = 0;
+    pthread_t thread_remux_last;
+    bool thread_remux_running_this = false;
+    bool thread_remux_running_last = false;
     while (true) {
         time_t time_now = time(NULL);
         struct tm tms_now;
@@ -685,119 +863,98 @@ int camera_recorder(struct camera const *const camera) {
         tms_future.tm_sec = 0;
         time_t time_future = mktime(&tms_future);
         time_t time_diff = time_future - time_now;
-        char duration[16];
-        if (snprintf(duration, 16, "%ld", time_diff + 10) < 0) {
-            pr_error_with_errno("Failed to generate duration string for camera of url '%s'", camera->url);
+        // char duration[16];
+        // if (snprintf(duration, 16, "%ld", time_diff + 10) < 0) {
+        //     pr_error_with_errno("Failed to generate duration string for camera of url '%s'", camera->url);
+        //     return 2;
+        // }
+        struct remux_thread_arg remux_thread_arg = {
+            .in = camera->url,
+            .out = path,
+            .duration = time_diff + 10
+        };
+        pr_warn("Recording from '%s' to '%s', duration %us\n", camera->url, path, remux_thread_arg.duration);
+        pthread_t thread_remux_this;
+        if (pthread_create(&thread_remux_this, NULL, remux_thread, (void *)&remux_thread_arg)) {
+            pr_error_with_errno("Faile to create pthread to record camera of url '%s'", camera->url);
             return 2;
         }
-        pr_warn("Recording from '%s' to '%s', duration %ss\n", camera->url, path, duration);
-        pid_t child = fork();
-        switch (child) {
-        case -1:
-            pr_error_with_errno("Failed to fork for camera recorder for camera of url '%s'", camera->url);
-            return 3;
-        case 0: {
-            if (dup2(fds.null, STDOUT_FILENO) < 0 || dup2(fds.null, STDERR_FILENO) < 0) {
-                exit(EXIT_FAILURE);
-            }
-            char ffmpeg[] = "/usr/bin/ffmpeg";
-            char url[PATH_MAX];
-            strncpy(url, camera->url, PATH_MAX);
-            char *argv[] = {
-                ffmpeg,
-                "-use_wallclock_as_timestamps", "1",
-                /* Input */
-                "-i", url,
-                /* Codec */
-                "-c", "copy",
-                /* Duration */
-                "-t", duration,
-                /* Output */
-                "-y", path,
-                /* Sentry */
-                NULL
-            };
-            execv(ffmpeg, argv);
-            if (dup2(fds.stdout_dup, STDOUT_FILENO) >= 0 && dup2(fds.stderr_dup, STDERR_FILENO) >=0) {
-                pr_error_with_errno("Failed to exec ffmpeg to record from url '%s'", camera->url);
-            }
-            exit(EXIT_FAILURE);
-        }
-        default:
-            break;
-        }
-        while (time_now < time_future) {
-            int status;
-            pid_t waited = waitpid(child, &status, WNOHANG);
-            switch (waited) {
-            case -1:
-                pr_error_with_errno("Failed to wait for child ffmpeg %d", child);
-                return 2;
+        thread_remux_running_this = true;
+        while (time_now < time_future && thread_remux_running_this) {
+            long ret;
+            int r = pthread_tryjoin_np(thread_remux_this, (void **)&ret);
+            switch (r) {
+            case EBUSY:
+                sleep(1);
+                break;
             case 0:
+                if (ret) {
+                    pr_error("Thread for remuxer of camera of url '%s' exited (with %ld) which is not expected, but we accept it\n", camera->url, ret);
+                }
+                thread_remux_running_this = false;
                 break;
             default:
-                if (waited == child) {
-                    child = 0;
-                    break;
-                } else {
-                    pr_error("Unexpected waited child: want %d but get %d\n", child, waited);
-                    return 3;
-                }
+                pr_error("Unexpected return from pthread_tryjoin_np: %d\n", r);
+                return 1;
             }
-            if (last_child) {
-                switch (waited = waitpid(last_child, &status, WNOHANG)) {
-                case -1:
-                    pr_error_with_errno("Failed to wait for last child ffmpeg %d", last_child);
-                    return 4;
+            if (thread_remux_running_last) {
+                switch ((r = pthread_tryjoin_np(thread_remux_last, (void **)&ret))) {
+                case EBUSY:
+                    sleep(1);
+                    break;
                 case 0:
+                    if (ret) {
+                        pr_error("Thread for remuxer of camera of url '%s' exited (with %ld) which is not expected, but we accept it\n", camera->url, ret);
+                    }
+                    thread_remux_running_last = false;
                     break;
                 default:
-                    if (waited == last_child) {
-                        last_child = 0;
-                        break;
-                    } else {
-                        pr_error("Unexpected waited last child: want %d but get %d\n", last_child, waited);
-                        return 5;
-                    }
+                    pr_error("Unexpected return from pthread_tryjoin_np: %d\n", r);
+                    return 1;
                 }
-            }
-            if (!child) {
-                break;
             }
             time_diff = time_future - (time_now = time(NULL));
             sleep(time_diff > 10 ? 10 : time_diff);
         }
-        if (child) {
-            if (last_child) {
-                int status;
-                pid_t waited = waitpid(last_child, &status, WNOHANG);
-                switch (waited) {
-                case -1:
-                    pr_error_with_errno("Failed to wait for last child ffmpeg %d", last_child);
-                    return 6;
+        if (thread_remux_running_this) {
+            if (thread_remux_running_last) {
+                long ret;
+                int r = pthread_tryjoin_np(thread_remux_last, (void **)&ret);
+                switch (r) {
+                case EBUSY:
+                    if (pthread_kill(thread_remux_last, SIGINT)) {
+                        pr_error("Faile to send kill signal to last remux thread for camera of url '%s'", camera->url);
+                        return 1;
+                    }
+                    sleep(1);
+                    switch ((r = pthread_tryjoin_np(thread_remux_last, (void **)&ret))) {
+                    case EBUSY:
+                        pr_error("Failed to kill pthread for last remux thread for camera of url '%s' for good", camera->url);
+                        return 1;
+                    case 0:
+                        if (ret) {
+                            pr_error("Thread for remuxer of camera of url '%s' exited (with %ld) which is not expected, but we accept it\n", camera->url, ret);
+                        }
+                        thread_remux_running_last = false;
+                        break;
+                    default:
+                        pr_error("Unexpected return from pthread_tryjoin_np: %d\n", r);
+                        return 1;
+                    }
+                    break;
                 case 0:
-                    if (kill(last_child, SIGINT)) {
-                        pr_error_with_errno("Failed to sent SIGINT to last child ffmpeg %d", last_child);
-                        return 7;
+                    if (ret) {
+                        pr_error("Thread for remuxer of camera of url '%s' exited (with %ld) which is not expected, but we accept it\n", camera->url, ret);
                     }
-                    if ((waited = waitpid(last_child, &status, 0)) <= 0) {
-                        pr_error_with_errno("Failed to wait for killed last child ffmpeg %d", last_child);
-                        return 8;
-                    }
-                    if (waited != last_child) {
-                        pr_error("Unexpected waited killed last child: want %d but get %d\n", last_child, waited);
-                        return 9;
-                    }
+                    thread_remux_running_last = false;
                     break;
                 default:
-                    if (waited != last_child) {
-                        pr_error("Unexpected waited last child: want %d but get %d\n", last_child, waited);
-                        return 10;
-                    }
-                    break;
+                    pr_error("Unexpected return from pthread_tryjoin_np: %d\n", r);
+                    return 1;
                 }
             }
-            last_child = child;
+            thread_remux_last = thread_remux_this;
+            thread_remux_running_last = true;
         }
     }
     return 0;
